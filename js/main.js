527fd7795212d7650b3fdaf56d59d26afac9af38
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-11 16:13:30 +0100

    feat add program untill Bonus 1

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// dichiaro e assegno costante contenitore delle card
+const cardContainer = document.getElementById("card-container");
+
+// ciclo for dove eseguire le iterazioni, controlli e stampa
+for (let i = 1; i <= 100; i++) {
+  let fizzPrint = 0;
+  let fizzClass = "";
+  // creiamo il contenitore dove creiamo il container delle card
+  const fizzCol = document.createElement("div");
+  fizzCol.classList.add("col-2");
+
+  cardContainer.append(fizzCol);
+
+  if (i % 15 == 0) {
+    fizzPrint = "FIZZBUZZ";
+    fizzClass = "fizzbuzz";
+  } else if (i % 5 == 0) {
+    fizzPrint = "BUZZ";
+    fizzClass = "buzz";
+  } else if (i % 3 == 0) {
+    fizzPrint = "FIZZ";
+    fizzClass = "fizz";
+  } else {
+    fizzPrint = i;
+    fizzClass = "number";
+  }
+
+  fizzCol.innerHTML += `
+    <div class="card ${fizzClass}">
+        <div class="card-body">
+            ${fizzPrint}
+        </div>
+    </div>
+  `;
+}
