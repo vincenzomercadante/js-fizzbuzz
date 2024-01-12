c463ce9d89598d11444ef08d63c7cbcf6d55c3e3
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-12 10:03:02 +0100

    feat add comments

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ const cardContainer = document.getElementById("card-container");
 
 // ciclo for dove eseguire le iterazioni, controlli e stampa
 for (let i = 1; i <= 100; i++) {
+  // inizializzo le mie varibiali che mi stamperanno il risultato e la classe con la quale assegnerò i bgcolor
   let fizzPrint = 0;
   let fizzClass = "";
-  // creiamo il contenitore dove creiamo il container delle card
+  // creiamo il contenitore dove andremo ad inserire le nostre card
   const fizzCol = document.createElement("div");
   fizzCol.classList.add("col");
-
+  // inseriamo nell'HTML la col
   cardContainer.append(fizzCol);
 
+  // verifica sul contatore
   if (i % 15 == 0) {
     fizzPrint = "FIZZBUZZ";
     fizzClass = "fizzbuzz";
@@ -25,9 +27,10 @@ for (let i = 1; i <= 100; i++) {
     fizzClass = "number";
   }
 
+  // stampa in HTML le card con all'interno i risultati
   fizzCol.innerHTML += `
     <div class="card ${fizzClass}">
-        <div class="card-body fw-bold">
+        <div class="card-body">
             ${fizzPrint}
         </div>
     </div>
